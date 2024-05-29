c9d28c7dd70ecf306ec56835c6633991713597b0
eric31512 <eric31512@gmail.com>
2024-05-29 14:47:58 +0800

    add fixed Power

--- fitFunction.m
+++ fitFunction.m
@@ -2,7 +2,9 @@ function  [fitvalue]=fitFunction(group)
     Max=[1260   , 15    , 103062960  , 252   , 1562275   , 495   , 31824     , 64    , 9         , 16    , 10    , 5];
     Min=[1      , 1     , 1          , 1     , 1         , 1     , 1         , 1     , 1         , 1     , 1     , 1];
     %    烘衣機  熱水壺    冷氣         電熱器   加壓馬達     烤箱    PHEV        洗碗機   煮飯機      洗衣機   微波爐  烤土司機
-
+    fixedPower = zeros(1,48);
+    fixedPower = fixedPower+0.04;
+    fixedPower = fixedPower+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,3.7,3.7,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,4,4,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5];
     Indx_max=group>Max;    %建立一個陣列其元素為group(i,:)是否大於MAX
     Indx_min=group<Min;    %建立一個陣列其元素為group(i,:)是否小於MIN
     group=(group.*(~(Indx_max+Indx_min)))+Max.*Indx_max+Min.*Indx_min;
@@ -11,6 +13,7 @@ function  [fitvalue]=fitFunction(group)
         curPower=Power(group(i),i); %將編號轉換為原始編碼型態
         totalPower = totalPower+curPower;   %將結果存入totalPower
     end
+    totalPower = fixedPower + totalPower;
     loadcheck = LoadCheck(totalPower);  %檢查是否超過電力負載
     if loadcheck == true
         fitvalue=inf;   %若發現則將此一族群的fitvalue設為無限大
